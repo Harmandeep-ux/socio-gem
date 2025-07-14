04919eb95dc7a0e4dbf2429c843c0ddb42de31a9
Harman sahota <sahotaharman59111@gmail.com>
2025-07-14 22:09:49 +0530

    Add Vercel domain to CORS

--- server/index.js
+++ server/index.js
@@ -9,9 +9,13 @@ const app = express()
  
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true,
-}))
+  origin: [
+    'http://localhost:5173', 
+    'https://socio-gem.vercel.app' 
+  ],
+  credentials: true,
+}));
+
 
 const PORT = process.env.PORT
 
