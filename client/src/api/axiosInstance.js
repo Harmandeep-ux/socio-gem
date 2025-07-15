3b7394c6b71da5bfa1a54f485d9ff35889939ceb
Harmandeep-ux <sahotaharman59111@gmail.com>
2025-07-15 14:27:24 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://socio-gem.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://socio-gem.onrender.com/api",
   withCredentials: true,
 });
 
